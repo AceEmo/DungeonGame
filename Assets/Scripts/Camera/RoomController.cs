42a5547639ce2ae76cf379079a80f334b45f1332
Emil Peev <emilpeev12345@gmail.com>
2026-02-26 19:53:17 +0200

    adding room camera and background

--- Assets/Scripts/Camera/RoomController.cs
+++ Assets/Scripts/Camera/RoomController.cs
@@ -1,28 +1,22 @@
 using UnityEngine;
-using Cinemachine;
+using Unity.Cinemachine;
 
 public class RoomController : MonoBehaviour
 {
-    // Тук ще влачим камерата на стаята в Unity
-    public CinemachineVirtualCamera roomCamera;
+    public CinemachineCamera roomCamera;
 
-    // Когато нещо влезе в квадрата на стаята
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // Ако това "нещо" има таг "Player"
         if (other.CompareTag("Player"))
         {
-            // Правим тази камера най-важна (Priority 10)
             roomCamera.Priority = 10;
         }
     }
 
-    // Когато нещо излезе от квадрата
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            // Спираме да я ползваме (Priority 0)
             roomCamera.Priority = 0;
         }
     }
